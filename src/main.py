86ff0f13ed2652ac15a3665c7ca3ef92c648b02a
SteiniDavid <steinidavid@gmail.com>
2016-05-19 11:56:21 -0400

    Asking input

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
 #First file!
+
+print "Integrates the following function in two ways."
+print "Enter Function"
+function = raw_input()
+print "Enter dx"
+dx = raw_input()
+print "Enter upperbound of integration"
+upbnd = raw_input()
+print "Enter lowerbound of integration"
+lwbnd = raw_input()
+
+print dx
+
+
