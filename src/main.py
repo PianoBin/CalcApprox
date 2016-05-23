b5bbc975fbe9e891e42208d9a0f507f38dd162b8
Shoji Moto <shmoto1@gmail.com>
2016-05-23 14:00:11 -0400

    String to array

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
+#FYI, raw_input was renamed to input in Python 3. That's a conflict between our versions.
+
 #First file!
 
 print ("Integrates the following function in two ways.")
-print ("Enter Function")
-function = raw_input()
-print ("Enter dx")
-dx = raw_input()
-print ("Enter upperbound of integration")
-upbnd = raw_input()
-print ("Enter lowerbound of integration")
-lwbnd = raw_input()
+prompt1 = "Enter Function: "
+function = input(prompt1)
+prompt2 = ("Enter dx: ")
+dx = input(prompt2)
+prompt3 = ("Enter upperbound of integration: ")
+upbnd = input(prompt3)
+prompt4 = ("Enter lowerbound of integration: ")
+lwbnd = input(prompt4)
 
 #Simpsons Method
 
@@ -28,17 +30,20 @@ def traprule (function, dx, upbnd, lwbnd):
 def Function_Evaluation (function):
 
 	listyMclistFace = []
-	leng_function = function.length
-	for x in function (0,leng_function):
-		theChar = 
-		listyMclistFace.append(x)
+	#leng_function = function.length
+	#for x in function (0,leng_function):
+	#	theChar = 
+	#	listyMclistFace.append(x)
 	
+	#instead, we can just split
+	listyMclistFace = function.split()
 
+	#testing
+	for theChar in listyMclistFace:
+		print (theChar)
 
-
-
-
-
+#testing, running the Function_Evaluation method
+Function_Evaluation(function)
 
 
 
