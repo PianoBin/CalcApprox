a7f05ffd08b2fac3e167cb7dc6eeecca2e88652e
Shoji Moto <shmoto1@gmail.com>
2016-05-24 13:40:49 -0400

    Trap/Simp's build

--- src/main.py
+++ src/main.py
@@ -1,6 +1,29 @@
 #FYI, raw_input was renamed to input in Python 3. That's a conflict between our versions.
+#The current implementation of the for loop in trapezoidal and simpson's is incorrect, does not reflect formula. Will fix.
 
-#First file!
+#INFO:
+'''
+This program will run two approximations for definite integrals:
+	1. Simpson's Rule
+		Partitioning the interval into sub-intervals and calculating the areas of each subinterval using parabolas.
+		Formula is AREA=((widthOfSubinterval)/2)(f[x(0)]+4f[x(1)]+2f[x(2)]+4f[x(3)]+2f[x(4)]+...+f[x(n)])
+	2. Trapezoidal Rule
+		Partitioning the interval into sub-intervals and calculating the areas of each subinterval using trapezoids.
+		Formula is AREA=((widthOfSubinterval)/2)(f[x(0)]+2f[x(1)]+2f[x(2)]+...+2f[x(n-1)]+f[x(n)])
+	widthOfSubinterval = (upper bound - lower bound) / number of subintervals
+	In Simpson's Rule, number of subintervals MUST BE EVEN
+'''
+
+#CHECKLIST:
+'''
+INPUT (DONE)
+Simpson's Rule
+Trapezoidal Rule
+FindX (DONE)
+Solve
+Graph
+Compare Error b/t Simpson's & Trapezoidal
+'''
 
 print ("Integrates the following function in two ways.")
 prompt1 = "Enter Function: "
@@ -12,39 +35,59 @@ upbnd = input(prompt3)
 prompt4 = ("Enter lowerbound of integration: ")
 lwbnd = input(prompt4)
 
-#Simpsons Method
-
-
-
-
-
-
-#Trapazoidal Rule
-def traprule (function, dx, upbnd, lwbnd):
-	range = (upbnd - lwbnd)
+#Simpson's Rule
+def simpRule (function, dx, upbnd, lwbnd):
+	range = upbnd - lwbnd
 	n = range / dx
-	h = (range)/n
+	h = range / n
+
+	listy = []
+	listy = function.split()
+
+	sum = 0
+	tempListy = []	
+	for num in range(lwbnd, upbnd, dx):
+		tempListy = FindX(listy, num) #change the list, replace x with num
+		sum += Solve(tempListy) #parse the function and solve now we have dx	
 
 
-#Function Evaluation
-def Function_Evaluation (function):
 
-	listyMclistFace = []
-	#leng_function = function.length
-	#for x in function (0,leng_function):
-	#	theChar = 
-	#	listyMclistFace.append(x)
+#Trapezoidal Rule
+def trapRule (function, dx, upbnd, lwbnd):
+	range = (upbnd - lwbnd)
+	n = range / dx #number of subintervals
+	h = range/n 
 	
-	#instead, we can just split
-	listyMclistFace = function.split()
+	#deleted Function_Evaluation, moved to here
+	listy = []
+	listy = function.split()
 
-	#testing
-	for theChar in listyMclistFace:
-		print (theChar)
+	sum = 0
+	tempListy = [] #do not modify the original listy while looping
+	for num in range(lwbnd, upbnd, dx):
+		tempListy = FindX(listy, num) #change the list, replace x with num
+		sum += Solve(tempListy) #parse the function and solve now we have dx	
 
-#testing, running the Function_Evaluation method
-Function_Evaluation(function)
+def FindX (listy, xitdx) : #Change the x to given number, modified listy 
+	for val in listy:
+		if listy[val].equals("x"):
+			listy[val] = xitdx
+	return listy
+
+def Solve (listy): #Find y
+	counter = 0
+	
+	if  listy[counter].equals("+"): #addition symbol
+				
+	elif listy[counter].equals("-"): #subtraction
+	
+	elif listy[counter].equals("*"): #multiplication
+
+	elif listy[counter].equals("/"): #division
+
+	elif  listy[counter].equals("^"): #power
+	
+	else : #Just a number
 
 
-
-
+	counter++
